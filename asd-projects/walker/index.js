d53a051a31da5d1de8eb74a3462293e8836fb002
yasseenmukhtar <142332258+yasseenmukhtar@users.noreply.github.com>
2025-02-13 16:07:02 +0000

    saves

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -33,16 +33,27 @@ function runProgram() {
     width: $("#walker").width(),
     height: $("#walker").height(),
   };
+  var walker2 = Walker("#walker", 0,0, 0, 0, WALKER_WIDTH, WALKER_HEIGHT);
+  var walker2 = Walker("#walker2", BOARD_WIDTH - WALKER_WIDTH, BOARD_HEIGHT - WALKER_HEIGHT, 0, 0, WALKER_WIDTH, WALKER_HEIGHT );
+  
+ 
 
-  var walker2 = {
-    xPos: BOARD_WIDTH - WALKER_WIDTH,
-    yPos: BOARD_HEIGHT - WALKER_HEIGHT,
-    speedX: 0,
-    speedY: 0,
-    width: $("#walker2").width(),
-    height: $("#walker2").height(),
-  };
-
+  function Walker(id, xPos, yPos, speedX, speedY, width, height){
+    let obj = {
+      id: id,
+      xPos: xPos,
+      yPos: yPos,
+      speedX: speedX,
+      speedY: speedY,
+      width: width,
+      height: height
+    };
+    return obj;
+  }
+  
+  
+  
+  
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL); // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on("keydown", handleKeyDown); // change 'eventType' to the type of event you want to handle
